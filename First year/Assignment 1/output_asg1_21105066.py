40974374ceb97da3c968ad4b62274041771575b6
vishvjeet-thakur <96479639+vishvjeet-thakur@users.noreply.github.com>
2022-01-22 20:16:25 +0530

    Update output_asg1_21105066.py

--- First year/Assignment 1/output_asg1_21105066.py
+++ First year/Assignment 1/output_asg1_21105066.py
@@ -1,7 +1,4 @@
-Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 
-==== RESTART: C:\Users\ankit\projects\helloworld\assignment-1\asg_1_code.py ====
 ASSIGNMENT 1
 
 Question 1
