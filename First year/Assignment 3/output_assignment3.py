aa8e45f8e5fdf9efef93f9f3915f2e255ee8a76e
vishvjeet-thakur <96479639+vishvjeet-thakur@users.noreply.github.com>
2022-02-09 15:54:25 +0530

    Add files via upload

---
+++ First year/Assignment 3/output_assignment3.py
@@ -0,0 +1,149 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\ankit\projects\helloworld\assignment 3\Assignment_3.py ===
+                                                      ASSIGNMENT 3 
+Question 1 :  To count the number of words in a string or characters . 
+
+Enter a string :  I will top this time definitely full I will top 100 this time
+Since word length is greater than 1 so checking for the number of same words :
+i            :2         
+will         :2         
+top          :2         
+this         :2         
+time         :2         
+definitely   :1         
+full         :1         
+100          :1         
+
+
+Question 2:- To print the next date of input date .
+
+Enter  year -2050
+
+Enter month -2
+
+Enter day -31
+
+Year you entered is  not a leap year . 
+ So February cannot have more than 28 days !!!!! 
+Please enter correct day again 
+
+Enter day -29
+
+Year you entered is  not a leap year . 
+ So February cannot have more than 28 days !!!!! 
+Please enter correct day again 
+
+Enter day -33
+Please input valid day !!!!!!
+
+Enter day -28
+
+
+Entered date is : 28/02/2050
+
+Next date is : 1/03/2050
+
+
+
+Question 3: to create a list of tuples having first element as the number and second as the square of the number. 
+
+Enter all the numbers in a single line separated by space :  1 2 3 4 7 5 9 10
+New list is :
+ [(1, 1), (2, 4), (3, 9), (4, 16), (7, 49), (5, 25), (9, 81), (10, 100)]
+
+
+Question 4: To print the grade and remark according to his/her grade .  
+
+Grade distribution is as :
+
+Letter Grade   Performance         Grade Point    
+A+             outstanding         10             
+A              Excellent           9              
+B+             Very Good           8              
+B              Good                7              
+C+             Average             6              
+C              Below Average       5              
+D              Poor                4              
+
+Enter your grade between 4 to 10 : 15
+Grade must be between 4 to 10 !!!! 
+ please enter a valid grade : 
+
+Enter your grade between 4 to 10 : 0
+Grade must be between 4 to 10 !!!! 
+ please enter a valid grade : 
+
+Enter your grade between 4 to 10 : 10
+
+Your Grade is A+ and outstanding Performance.
+
+
+Question 5: Pattern printing.
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+Question 6 : Dictionary functions and sorting.
+
+Enter name : vishvjeet Thakur
+Enter  SID  :21105066
+
+Do you want to store more student data (Y/N) : 
+y
+Enter name : ankit
+Enter  SID  :21105067
+
+Do you want to store more student data (Y/N) : 
+y
+Enter name : sourabh 21105069
+Enter  SID  :21105067
+
+Do you want to store more student data (Y/N) : 
+n
+
+Student data in the order as entered by the user : 
+21105066  :  vishvjeet Thakur
+21105067  :  sourabh 21105069
+
+Sorted dictionary according to student names :
+21105067       :sourabh 21105069
+21105066       :vishvjeet Thakur
+
+Sorted dictionary according to student sids :
+21105066  :  vishvjeet Thakur
+21105067  :  sourabh 21105069
+
+Enter the SID whose name you want to search :21105070
+SID 21105070 is not present in our data.
+
+
+Question 7 :  Printing of Fibonacci series .
+Enter the number of terms to which you want the Fibonacci Series :20
+
+1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 
+
+Average is  0.1
+
+
+Question 8 :  Set methods -  union , intersection , difference .
+Given sets are :
+Set1 =  {1, 2, 3, 4, 5}
+Set2 =  {8, 2, 4, 6}
+Set3 =  {1, 17, 5, 9, 13}
+
+New set of all elements that are in Set1 and Set2 but not both :  {1, 3, 5, 6, 8}
+
+New set of all elements that are in only one of the three sets Set1, Set2 and Set3 : {3, 6, 8, 9, 13, 17}
+
+New set of elements that are exactly two of the sets Set1, Set2 and Set3 :  {1, 2, 4, 5}
+
+New set of all integers in the range 1 to 10 that are not in Set1 : {6, 7, 8, 9, 10}
+
+New set of all integers in the range 1 to 10 that are not in Set1,Set2 and Set3: {10, 7}
