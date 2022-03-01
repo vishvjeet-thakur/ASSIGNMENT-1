9672afa8d8dde51808e94b6c9cbdbe0d0116ec2e
vishvjeet-thakur <96479639+vishvjeet-thakur@users.noreply.github.com>
2022-03-02 00:23:04 +0530

    Add files via upload

---
+++ assign_4_output.py
@@ -0,0 +1,108 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\ankit\projects\helloworld\Assignment4\assignment_4.py ===
+
+Question 1. Tower of Hanoi 
+
+Number of discs : 3
+Shift disc 1 from A to C
+Shift disc 2 from A to B
+Shift disc 1 from C to B
+Shift disc 3 from A to C
+Shift disc 1 from B to A
+Shift disc 2 from B to C
+Shift disc 1 from A to C
+
+Minimum number of shifts required to move the arrangement of 3 from 'A' to 'C'  is  7
+
+ Question 2 . Pascals Triangle 
+
+Enter number of rows: 5
+BY recursion :
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+By iteration :
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+
+Question 3. Use of some inbuilt function -> callable, hash ,set methods etc.
+
+Enter First number : 10
+Enter Second number : 6
+Quotient obtained is  : 1
+Remainder obtained is : 4
+
+a. Check whether function/values is callable or not .
+
+Function used is callable
+Value 10 is not callable 
+Value 6 is not callable 
+
+b. Check whether all the values are non zeroes or not .
+
+None of the given values and the quotient and remainder obtaineed is zero .
+
+c. Add values (4, 5, 6) to the result and filter out the values which are greater than 4.
+
+The filtered values which are greater than 4 are  : [5, 6, 6, 10]
+
+d. Convert the above result into set datatype.
+
+The result that is of values greater than 4 is converted to set : {10, 5, 6}
+
+e. Make that set immutable.
+
+Set has been made immutable.
+
+f. Evaluate the maximum value from set and find out its hash value
+
+Maximum value in  the set is  10
+
+Hash values for the  maximum value according to the considerration :
+if it is considered as a integer then : 10
+if it is considered as a string then : -1214220614461833776
+
+Question 4. Function related to OOPs concepts .
+
+Enter your name: vishvjeet thakur
+Enter your Roll number: 21105066
+
+Name is : vishvjeet thakur
+Roll number is : 21105066
+
+
+Question 5. To store  details of empoyees and update their info.
+
+Name          Salary  
+
+Mehak         40000   
+Ashok         50000   
+Viren         60000   
+
+Salary of Mehak named person  is updated to 70000 !!!
+
+Name          Salary  
+
+Mehak         70000   
+Ashok         50000   
+Viren         60000   
+record of "Viren" named person is deleted from data !!!
+Name          Salary  
+
+Mehak         70000   
+Ashok         50000   
+
+Question 6. To check if friendship is true or not .
+
+Enter your word George :rmslt
+Enter your word Barbie and remember it must be consistig of same words as George word letters only :  Stmrl
+
+Barbie your word  contains same  letters as George .
+So your friendship is true.
